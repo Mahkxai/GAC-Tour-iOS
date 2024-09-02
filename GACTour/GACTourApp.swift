473ecedb1f008a7b515032070dd5a266d93de36f
Hardik Shrestha <shr.hardik@gmail.com>
2024-09-01 21:05:58 -0500

    Initial Commit

---
+++ GACTour/GACTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  GACTourApp.swift
+//  GACTour
+//
+//  Created by Hardik Shrestha on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GACTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
